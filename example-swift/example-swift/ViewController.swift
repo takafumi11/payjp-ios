1c33dd0f1014959577bb97e29e2ef2e235261799
yatatsu <yatatsukitagawa@gmail.com>
2018-08-21 17:26:26 +0900

    Fix sample code warning.

--- example-swift/example-swift/ViewController.swift
+++ example-swift/example-swift/ViewController.swift
@@ -55,7 +55,7 @@ class ViewController: UITableViewController {
         let month = fieldCardMonth.text ?? ""
         let year = fieldCardYear.text ?? ""
         let name = filedCardName.text
-        print("input number=\(number), cvc=\(cvc), month=\(month), year=\(year) name=\(name)")
+        print("input number=\(number), cvc=\(cvc), month=\(month), year=\(year) name=\(name ?? "nil")")
         payjpClient.createToken(
             with: number,
             cvc: cvc,
