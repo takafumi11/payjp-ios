3ee2ae3344b6c00f4d2aa59f1976aefb33d58969
vc7 <vince78718@gmail.com>
2019-07-12 16:04:26 +0900

    Composing the user agent.

--- Sources/UserAgent.swift
+++ Sources/UserAgent.swift
@@ -6,10 +6,25 @@
 //  Copyright © 2019 PAY, Inc. All rights reserved.
 //
 
-import Foundation
+import UIKit
 
 struct UserAgent {
     static var `default`: String {
-        return "jp.pay.ios"
+        let device = UIDevice.current
+        let version = Bundle(for: APIClient.self).infoDictionary?["CFBundleShortVersionString"] as? String ?? ""
+        let osVersion = device.systemVersion
+        let deviceName = self.device
+        return "jp.pay.ios/\(version); iOS/\(osVersion); apple; \(deviceName)"
+    }
+    
+    private static var device: String {
+        var systemInfo = utsname()
+        uname(&systemInfo)
+        let machineMirror = Mirror(reflecting: systemInfo.machine)
+        let identifier = machineMirror.children.reduce("") { identifier, element in
+            guard let value = element.value as? Int8, value != 0 else { return identifier }
+            return identifier + String(UnicodeScalar(UInt8(value)))
+        }
+        return identifier
     }
 }
