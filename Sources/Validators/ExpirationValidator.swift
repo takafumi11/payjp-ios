de000c68d129ab26e112d318dcda9dcc521fce42
vc7 <vince78718@gmail.com>
2019-07-19 16:57:28 +0900

    Add logics and the tests.

--- Sources/Validators/ExpirationValidator.swift
+++ Sources/Validators/ExpirationValidator.swift
@@ -7,3 +7,39 @@
 //
 
 import Foundation
+
+protocol ExpirationValidatorType {
+    func isValid(month: String, year: String) -> Bool
+}
+
+struct ExpirationValidator: ExpirationValidatorType {
+    
+    static let shared = ExpirationValidator()
+    
+    func isValid(month: String, year: String) -> Bool {
+        guard let intMonth = Int(month), let intYear = Int(year) else { return false }
+        
+        let moddedIntYear = intYear % 100
+        
+        let currentMonth = self.currentMonth()
+        let currentYear = self.currentYear()
+        
+        if moddedIntYear == currentYear {
+            return intMonth >= currentMonth ? true : false
+        } else {
+            return moddedIntYear > currentYear ? true : false
+        }
+    }
+    
+    private func currentYear() -> Int {
+        let calendar = Calendar(identifier: .gregorian)
+        let year = calendar.component(.year, from: Date())
+        return year % 100
+    }
+    
+    private func currentMonth() -> Int {
+        let calendar = Calendar(identifier: .gregorian)
+        let month = calendar.component(.month, from: Date())
+        return month
+    }
+}
