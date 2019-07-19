c9894113a48911f81556cd5d579a931040a58829
vc7 <vince78718@gmail.com>
2019-07-19 16:14:16 +0900

    Add empty files.

---
+++ Sources/Validators/ExpirationValidator.swift
@@ -0,0 +1,9 @@
+//
+//  ExpirationValidator.swift
+//  PAYJP
+//
+//  Created by Li-Hsuan Chen on 2019/07/19.
+//  Copyright © 2019 PAY, Inc. All rights reserved.
+//
+
+import Foundation
---
+++ Tests/Validators/ExpirationValidatorTests.swift
@@ -0,0 +1,12 @@
+//
+//  ExpirationValidatorTests.swift
+//  PAYJPTests
+//
+//  Created by Li-Hsuan Chen on 2019/07/19.
+//  Copyright © 2019 PAY, Inc. All rights reserved.
+//
+
+import XCTest
+
+class ExpirationValidatorTests: XCTestCase {
+}
