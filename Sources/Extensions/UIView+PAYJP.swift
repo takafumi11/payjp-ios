24b7306e0e2ed59ecefdd784d980645bb11d533f
vc7 <vince78718@gmail.com>
2019-08-09 14:46:06 +0900

    Add UIView's extension to fetch its view controller.

--- Sources/Extensions/UIView+PAYJP.swift
+++ Sources/Extensions/UIView+PAYJP.swift
@@ -7,3 +7,16 @@
 //
 
 import Foundation
+
+extension UIView {
+    var parentViewController: UIViewController? {
+        var parentResponder: UIResponder? = self
+        while parentResponder != nil {
+            parentResponder = parentResponder!.next
+            if let viewController = parentResponder as? UIViewController {
+                return viewController
+            }
+        }
+        return nil
+    }
+}
