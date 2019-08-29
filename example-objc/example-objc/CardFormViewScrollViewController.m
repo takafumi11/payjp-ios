144afb7a297b8928092a43f4b6d8770cbb86a5f9
Tadashi Wakayanagi <wakayanagi_t@binc.jp>
2019-08-29 11:20:58 +0900

    add example with ScrollView

---
+++ example-objc/example-objc/CardFormViewScrollViewController.m
@@ -0,0 +1,51 @@
+//
+//  CardFormViewScrollViewController.m
+//  example-objc
+//
+//  Created by Tadashi Wakayanagi on 2019/08/28.
+//  Copyright © 2019 PAY, Inc. All rights reserved.
+//
+
+#import "CardFormViewScrollViewController.h"
+@import PAYJP;
+
+@interface CardFormViewScrollViewController ()
+
+@property (weak, nonatomic) IBOutlet PAYCardFormView *cardFormView;
+    @property (weak, nonatomic) IBOutlet UIButton *createTokenButton;
+    
+- (IBAction)cardHolderSwitchChanged:(id)sender;
+- (IBAction)createToken:(id)sender;
+- (IBAction)validateAndCreateToken:(id)sender;
+    
+@end
+
+@implementation CardFormViewScrollViewController
+    
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    self.cardFormView.delegate = self;
+}
+    
+- (void)inputValidated {
+    BOOL isValid = [self.cardFormView isValid];
+    [self.createTokenButton setEnabled:isValid];
+}
+
+- (IBAction)cardHolderSwitchChanged:(id)sender {
+    self.cardFormView.isHolderRequired = [sender isOn];
+}
+    
+- (IBAction)createToken:(id)sender {
+    // TODO: call createToken
+}
+    
+- (IBAction)validateAndCreateToken:(id)sender {
+    BOOL isValid = [self.cardFormView validateCardForm];
+    if (isValid) {
+        // TODO: call createToken
+    }
+}
+
+@end
