548119c101d769aa416dae01a085b1f38b610341
Tadashi Wakayanagi <wakayanagi_t@binc.jp>
2019-10-09 17:11:07 +0900

    fix sample

--- example-objc/example-objc/CardFormViewScrollViewController.m
+++ example-objc/example-objc/CardFormViewScrollViewController.m
@@ -15,6 +15,7 @@
 @property (weak, nonatomic) IBOutlet PAYCardFormLabelStyledView *cardFormView;
 @property (weak, nonatomic) IBOutlet UIButton *createTokenButton;
 @property (weak, nonatomic) IBOutlet UITextField *selectColorField;
+@property (weak, nonatomic) IBOutlet UILabel *tokenIdLabel;
 
 - (IBAction)cardHolderSwitchChanged:(id)sender;
 - (IBAction)createToken:(id)sender;
@@ -115,14 +116,77 @@
 }
     
 - (IBAction)createToken:(id)sender {
-    // TODO: call createToken
+    if (!self.cardFormView.isValid) {
+        return;
+    }
+    [self createToken];
 }
     
 - (IBAction)validateAndCreateToken:(id)sender {
     BOOL isValid = [self.cardFormView validateCardForm];
     if (isValid) {
-        // TODO: call createToken
+        [self createToken];
     }
+}
+
+- (void)createToken {
+    __weak typeof(self) wself = self;
+    
+    [self.cardFormView createTokenWith:nil
+                            completion:
+     ^(PAYToken *token, NSError *error) {
+         if (error.domain == PAYErrorDomain && error.code == PAYErrorServiceError) {
+             id<PAYErrorResponseType> errorResponse = error.userInfo[PAYErrorServiceErrorObject];
+             NSLog(@"[errorResponse] %@", errorResponse.description);
+         }
+         
+         if (!token) {
+             dispatch_async(dispatch_get_main_queue(), ^{
+                 wself.tokenIdLabel.text = nil;
+                 [wself showError:error];
+             });
+             return;
+         }
+         
+         NSLog(@"token = %@", [wself displayToken:token]);
+         dispatch_async(dispatch_get_main_queue(), ^{
+             wself.tokenIdLabel.text = token.identifer;
+             [wself showToken:token];
+         });
+     }];
+}
+
+#pragma MARK: - Alert
+
+- (void)showToken:(PAYToken *)token {
+    UIAlertController *alert = [UIAlertController
+                                alertControllerWithTitle:@"success"
+                                message:[self displayToken:token]
+                                preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction
+                      actionWithTitle:@"OK"
+                      style:UIAlertActionStyleCancel
+                      handler:nil]];
+    [self presentViewController:alert animated:true completion:nil];
+}
+
+- (void)showError:(NSError *)error {
+    UIAlertController *alert = [UIAlertController
+                                alertControllerWithTitle:@"error"
+                                message:error.localizedDescription
+                                preferredStyle:UIAlertControllerStyleAlert];
+    [alert addAction:[UIAlertAction
+                      actionWithTitle:@"OK"
+                      style:UIAlertActionStyleCancel
+                      handler:nil]];
+    [self presentViewController:alert animated:true completion:nil];
+}
+
+#pragma MARK: - misc
+
+- (NSString *)displayToken:(PAYToken *)token {
+    return [NSString stringWithFormat:@"id=%@,\ncard.id=%@,\ncard.last4=%@,\ncard.exp=%hhu/%hu\ncard.name=%@",
+            token.identifer, token.card.identifer, token.card.last4Number, token.card.expirationMonth, token.card.expirationYear, token.card.name];
 }
 
 @end
