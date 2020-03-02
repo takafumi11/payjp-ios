aed33d519444b0d9805073d9c143843d778aff4a
yatatsu <yatatsukitagawa@gmail.com>
2020-03-02 12:03:53 +0900

    Bump up to 1.1.6

--- Sources/Core/PAYJPSDK.swift
+++ Sources/Core/PAYJPSDK.swift
@@ -43,7 +43,7 @@ public final class PAYJPSDK: NSObject, PAYJPSDKType {
     public static var clientInfo: ClientInfo = .default
 
     // Update by Fastlane :bump_up_version
-    public static let sdkVersion: String = "1.1.5"
+    public static let sdkVersion: String = "1.1.6"
 
     static var authToken: String = ""
 }
