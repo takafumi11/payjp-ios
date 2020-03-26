dd7a538bc2f3b65cc1de7cf08b4b58706ee8fef3
tdc-w <wakayanagi_t@binc.jp>
2020-03-26 16:10:07 +0900

    add test

---
+++ Tests/Networking/Models/ThreeDSecureTokenTests.swift
@@ -0,0 +1,25 @@
+//
+//  ThreeDSecureTokenTests.swift
+//  PAYJPTests
+//
+//  Created by Tadashi Wakayanagi on 2020/03/26.
+//  Copyright © 2020 PAY, Inc. All rights reserved.
+//
+
+import XCTest
+@testable import PAYJP
+
+class ThreeDSecureTests: XCTestCase {
+
+    func testTdsEntryUrl() {
+        let tdsId = "tds_1234"
+        let token = ThreeDSecureToken(identifier: tdsId)
+        XCTAssertEqual(token.tdsEntryUrl?.absoluteString, "\(PAYJPApiEndpoint)tds/\(tdsId)/start")
+    }
+
+    func testTdsFinishUrl() {
+        let tdsId = "tds_1234"
+        let token = ThreeDSecureToken(identifier: tdsId)
+        XCTAssertEqual(token.tdsFinishUrl?.absoluteString, "\(PAYJPApiEndpoint)tds/\(tdsId)/finish")
+    }
+}
