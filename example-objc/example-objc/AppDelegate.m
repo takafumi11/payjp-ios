9fbaa623b25e03ea1f09446c13690e346cd90d80
tdc-w <wakayanagi_t@binc.jp>
2020-04-06 13:04:52 +0900

    reformat

--- example-objc/example-objc/AppDelegate.m
+++ example-objc/example-objc/AppDelegate.m
@@ -33,8 +33,8 @@ NSString *const App3DSRedirectURLKey = @"ios-app";
 - (BOOL)application:(__unused UIApplication *)app
             openURL:(NSURL *)url
             options:(NSDictionary<NSString *, id> *)options {
-  BOOL result = [[PAYJPThreeDSecureProcessHandler sharedHandler]
-      completeThreeDSecureProcessWithUrl:url];
+  BOOL result =
+      [[PAYJPThreeDSecureProcessHandler sharedHandler] completeThreeDSecureProcessWithUrl:url];
   return result;
 }
 
