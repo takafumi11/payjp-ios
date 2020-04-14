a6d3d37bfeefcc8181239503db7b913be2256338
yatatsu <yatatsukitagawa@gmail.com>
2020-04-14 15:38:13 +0900

    Define other tds statuses.

--- Sources/Networking/Models/ThreeDSecureStatus.swift
+++ Sources/Networking/Models/ThreeDSecureStatus.swift
@@ -11,12 +11,18 @@ class ThreeDSecureStatus {
 
     static func find(rawValue: String) -> PAYThreeDSecureStatus? {
         switch rawValue {
-        case PAYThreeDSecureStatus.verified.rawValue:
-            return PAYThreeDSecureStatus.verified
-        case PAYThreeDSecureStatus.attempted.rawValue:
-            return PAYThreeDSecureStatus.attempted
         case PAYThreeDSecureStatus.unverified.rawValue:
             return PAYThreeDSecureStatus.unverified
+        case PAYThreeDSecureStatus.verified.rawValue:
+            return PAYThreeDSecureStatus.verified
+        case PAYThreeDSecureStatus.failed.rawValue:
+            return PAYThreeDSecureStatus.failed
+        case PAYThreeDSecureStatus.attempted.rawValue:
+            return PAYThreeDSecureStatus.attempted
+        case PAYThreeDSecureStatus.aborted.rawValue:
+            return PAYThreeDSecureStatus.aborted
+        case PAYThreeDSecureStatus.error.rawValue:
+            return PAYThreeDSecureStatus.error
         default:
             return nil
         }
