fc5e8e7028502d272fe3e7acda0f27a52b7a3d87
yatatsu <yatatsukitagawa@gmail.com>
2020-09-30 00:23:00 +0900

    Bump up to 1.3.0

--- Sources/Core/PAYJPSDK.swift
+++ Sources/Core/PAYJPSDK.swift
@@ -47,7 +47,7 @@ public final class PAYJPSDK: NSObject, PAYJPSDKType {
     public static var clientInfo: ClientInfo = .default
 
     // Update by Fastlane :bump_up_version
-    public static let sdkVersion: String = "1.2.5"
+    public static let sdkVersion: String = "1.3.0"
 
     static var authToken: String = ""
 }
--- podspec.rb
+++ podspec.rb
@@ -1,5 +1,5 @@
 module PAYJPSDK
-  VERSION = '1.2.5'
+  VERSION = '1.3.0'
   HOMEPAGE_URL = 'https://github.com/payjp/payjp-ios'
   LICENSE = { :type => 'MIT' }
   AUTHOR = { 'PAY.JP (https://pay.jp)' => 'support@pay.jp' }
